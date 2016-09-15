93bafcaddaa43467a68d9f3785f46131c542bff5
zassmin <zmontesd@gmail.com>
2016-09-15 05:57:42 -0700

    add static html

--- meetupgoers.go
+++ meetupgoers.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+  "os"
+)
 
 func main() {
-  fmt.Println("Hello MeetupGoers")	
+  http.HandleFunc("/", root)
+  http.ListenAndServe(GetPort(), nil)	
+}
+
+// Get the Port from the environment so we can run on Heroku
+func GetPort() string {
+        var port = os.Getenv("PORT")
+        // Set a default port if there is nothing in the environment
+        if port == "" {
+                port = "8080"
+                fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+        }
+        return ":" + port
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Meetup Go'ers")
 }
